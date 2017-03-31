2a88e019580139cc893231e356a94806265cf5e3
jrb688 <jrb688@nau.edu>
2017-03-30 21:45:13 -0700

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author jrbak
+ * @author Josh Baker and Nathan Franklin
  */
 public class MainProgram extends javax.swing.JFrame {
 
